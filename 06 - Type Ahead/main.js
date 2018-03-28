ef9ca00a27ab84564d90b6b68d7cbef4841e9079
jsc215 <jeffcomertennis@gmail.com>
2018-03-27 20:30:36 -0400

    type ahead

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -23,10 +23,10 @@ function displayMatches(){
   	const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
   	const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
   	return `
-  		<li>
-  			<span class="name">${cityName}, ${stateName}</span>
-  			<span class="population">${numberWithCommas(place.population)}</span>
-  		</li>
+		  <li>
+        <span class="name">${cityName}, ${stateName}</span>
+	      <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
   	`;
   }).join('');
 
