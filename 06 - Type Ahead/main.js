f01cbcd6b41492a13eb963095e6da889257afb77
jsc215 <jeffcomertennis@gmail.com>
2018-03-27 20:36:52 -0400

    type ahead

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -19,10 +19,10 @@ function numberWithCommas(x) {
 function displayMatches(){
   const matchArray = findMatches(this.value, cities);
   const html = matchArray.map(place => {
-  	const regex = new RegExp(this.value, 'gi');
-  	const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-  	const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-  	return(
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return(
       `<li>
         <span class="name">${cityName}, ${stateName}</span>
         <span class="population">${numberWithCommas(place.population)}</span>
